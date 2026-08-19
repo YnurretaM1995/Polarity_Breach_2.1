70a1660adffbe7fc7b2974384a6519733d213248
RobbieSR <robbiesutherlandrusso@gmail.com>
2026-08-19 17:04:21 -0400

    Hooked the post processing effect to the PlayerPolarityController

--- Assets/Scripts/PolaritySystem/PlayerPolarityController.cs
+++ Assets/Scripts/PolaritySystem/PlayerPolarityController.cs
@@ -20,6 +20,7 @@ namespace PolarityBreach.PolaritySystem
 
         [SerializeField] private AudioClip colorSound;
         [SerializeField] private AbilityUIDisplay polarityUI;
+        [SerializeField] private PolarityPostProcessPulse postProcessPulse;
         public event Action OnPolaritySwitched;
         public static event Action<Transform> OnAnyPlayerPolaritySwitched;
         
@@ -57,6 +58,16 @@ namespace PolarityBreach.PolaritySystem
         {
             _polarity = GetComponent<PolarityComponent>();
             _playerStats = GetComponent<PlayerStatsData>();
+
+            if (postProcessPulse == null)
+            {
+                postProcessPulse = GetComponentInChildren<PolarityPostProcessPulse>();
+            }
+
+            if (postProcessPulse == null)
+            {
+                postProcessPulse = FindFirstObjectByType<PolarityPostProcessPulse>();
+            }
         }
 
         private void OnEnable()
@@ -84,6 +95,7 @@ namespace PolarityBreach.PolaritySystem
         {
             if (!CanSwitch) return false;
             _polarity.Toggle();
+            postProcessPulse?.Play(_polarity.CurrentPolarity);
             _lastSwitchTime = Time.time;
             AudioHandler.Play3DSound(colorSound, transform.position);
             polarityUI.StartCooldownUI();
